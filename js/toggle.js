cfc9b261f6ca286935e25d2613ca11fc83e86466
rafi_ahmed <rafiiitju282@gmail.com>
2024-09-23 01:51:07 +0600

    added toggle buttons feature

--- js/toggle.js
+++ js/toggle.js
@@ -1,12 +1,23 @@
-valueById('history')
-.addEventListener('click', function(){
-    const cards = valueById('cards')
-    cards.classList.add('hidden')
-})
+valueById("history").addEventListener("click", function () {
+  const cards = valueById("cards");
+  cards.classList.add("hidden");
+  valueById("history").classList.add('bg-[#B4F461]')
+  valueById("history").classList.add("border-[#B4F461]");
+  valueById("history").style.color = "#111111";
+  valueById("donation").classList.remove('bg-[#B4F461]')
+  valueById("donation").classList.add('btn-outline')
+  valueById("donation").classList.add('border-2')
+  valueById("donation").classList.add('border-[#11111170]')
+});
 
-valueById('donation')
-.addEventListener('click', function(){
-    const cards = valueById('cards')
-    cards.classList.remove('hidden')
-
-})
+valueById("donation").addEventListener("click", function () {
+  const cards = valueById("cards");
+  cards.classList.remove("hidden");
+  valueById("history").classList.remove('bg-[#B4F461]')
+  valueById("history").classList.remove("border-[#B4F461]");
+  
+  valueById("donation").classList.add('bg-[#B4F461]')
+  valueById("donation").classList.remove('btn-outline')
+  valueById("donation").classList.remove('border-2')
+  valueById("donation").classList.remove('border-[#11111170]')
+});
