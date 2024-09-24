1e4b30f6ff5baca47207619738cbec0e727903ef
rafi_ahmed <rafiiitju282@gmail.com>
2024-09-25 02:16:14 +0600

    added rebuilding section

---
+++ js/rebuilding.js
@@ -0,0 +1,34 @@
+const rebuildingBtn = document.getElementById("rebuilding-btn");
+document.getElementById("rebuilding-btn").addEventListener("click", function () {
+  let navValue = getElementInnerHtmlById("nav-value");
+  let rebuildingInput = inputValue("rebuilding-input");
+  let rebuildingInputwithoutValue = valueById("rebuilding-input");
+  let rebuildingValue = getElementInnerHtmlById("rebuilding-value");
+  if (!isNaN(rebuildingInputwithoutValue.value)) {
+    if (navValue >= rebuildingInput && rebuildingInput > 0) {
+    let rebuildingTotal = add(rebuildingValue, rebuildingInput);
+    let remainingNavValue = substract(navValue, rebuildingInput);
+    valueById("rebuilding-value").innerHTML = rebuildingTotal;
+    valueById("nav-value").innerText = remainingNavValue;
+    // append child
+    let rebuildingTitle = valueById("rebuilding-title").innerText;
+    const div = document.createElement("div");
+    div.innerHTML += `
+    <h1 class="text-xl font-bold text-[#111111] mb-4">${rebuildingInput} Taka is Donated for ${rebuildingTitle}</h1>
+    <h2 class="text-lg font-light text-[#111111]">${currentDate()}</h2>
+    `;
+    let historyContainer = valueById("history-container");
+    div.classList.add("p-8");
+    div.classList.add("border-2");
+    div.classList.add("rounded-lg");
+
+    historyContainer.appendChild(div);
+    rebuildingBtn.setAttribute("onclick", my_modal_1.showModal());
+    }
+    else {
+      alert("Invalid Donation Amount");
+    }
+  } else {
+    alert("Invalid Donation Amount");
+  }
+});
